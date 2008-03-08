aafaea758e769613b8e68501bba287bdb9b1c8b7
Gregory Williams <samofool@tiu.local>
2008-03-07 22:28:18 -0500

    - Updated docs and signature for 0.102 release.

--- RDF-Trine/lib/RDF/Trine.pm
+++ RDF-Trine/lib/RDF/Trine.pm
@@ -11,7 +11,7 @@ RDF::Trine - An RDF Framework for Perl.
 
 =head1 VERSION
 
-This document describes RDF::Trine version 0.101
+This document describes RDF::Trine version 0.102
 
 =head1 SYNOPSIS
 
@@ -34,7 +34,7 @@ use warnings;
 no warnings 'redefine';
 
 BEGIN {
-	our $VERSION	= '0.101';
+	our $VERSION	= '0.102';
 }
 
 use RDF::Trine::Parser;
