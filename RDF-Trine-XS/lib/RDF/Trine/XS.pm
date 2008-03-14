e9fe8b3c68b2485ea1dd65a2735684c27a0891ed
Gregory Williams <samofool@tiu.local>
2008-03-14 16:06:21 -0400

    - Fixed RDF::Trine::XS to support unicode literals.

--- RDF-Trine-XS/lib/RDF/Trine/XS.pm
+++ RDF-Trine-XS/lib/RDF/Trine/XS.pm
@@ -5,13 +5,14 @@ use warnings;
 
 use XSLoader;
 use Digest::MD5 ('md5');
+use Encode;
 
 our $VERSION = '2.000_01';
 XSLoader::load "RDF::Trine::XS", $VERSION;
 
 sub hash {
 	my $value	= shift;
-	my $md5		= md5( $value );
+	my $md5		= md5( encode('utf8', $value) );
 	return _hash( $md5 );
 }
 
