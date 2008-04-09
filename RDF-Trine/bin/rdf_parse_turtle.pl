7d254d78bd7186a36903a1522fed0693e5d88c3e
Gregory Williams <samofool@kalee-01.dynamic2.rpi.edu>
2008-04-09 14:28:50 -0400

    - Updated example turtle parser to accept URLs as command line arguments.

--- RDF-Trine/bin/rdf_parse_turtle.pl
+++ RDF-Trine/bin/rdf_parse_turtle.pl
@@ -5,15 +5,24 @@ use warnings;
 no warnings 'redefine';
 use File::Spec;
 use lib qw(lib);
+use Data::Dumper;
 use FindBin qw($Bin);
+use LWP::Simple qw(get);
 use RDF::Trine::Parser::Turtle;
 
-my $file	= File::Spec->rel2abs( shift );
-my $data	= do {
-				open( my $fh, '<:utf8', $file );
-				local($/) = undef;
-				<$fh>
-			};
-my $url		= 'file://' . $file;
+my $url		= shift;
+my $data;
+if ($url =~ m#^http://#) {
+	$data	= get($url);
+} else {
+	my $file	= File::Spec->rel2abs( $url );
+	$data	= do {
+					open( my $fh, '<:utf8', $file );
+					local($/) = undef;
+					<$fh>
+				};
+	$url		= 'file://' . $file;
+}
+
 my $parser	= RDF::Trine::Parser::Turtle->new;
-$parser->parse( $url, $data );
+$parser->parse( $url, $data, sub { my $st = shift; print $st->as_string . "\n" } );
