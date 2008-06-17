3a159b2fef146aafd3d24a87b0ebcf386306b733
Gregory Todd Williams <greg@evilfunhouse.com>
2008-06-17 14:04:34 -0400

    - Updated args to roqet call in failing_earl_tests.sh.

--- RDF-Query/bin/failing_earl_tests.sh
+++ RDF-Query/bin/failing_earl_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-roqet -q -s earl-syntax.ttl -e 'PREFIX earl: <http://www.w3.org/ns/earl#> SELECT ?test WHERE { [ earl:test ?test ; earl:result [ earl:outcome earl:fail ] ] }' | cut -d '<' -f 2 | cut -d '>' -f 1
-roqet -q -s earl-eval.ttl -e 'PREFIX earl: <http://www.w3.org/ns/earl#> SELECT ?test WHERE { [ earl:test ?test ; earl:result [ earl:outcome earl:fail ] ] }' | cut -d '<' -f 2 | cut -d '>' -f 1
+roqet -q -e 'PREFIX earl: <http://www.w3.org/ns/earl#> SELECT ?test FROM <earl-syntax.ttl> WHERE { [ earl:test ?test ; earl:result [ earl:outcome earl:fail ] ] }' | cut -d '<' -f 2 | cut -d '>' -f 1
+roqet -q -e 'PREFIX earl: <http://www.w3.org/ns/earl#> SELECT ?test FROM <earl-eval.ttl> WHERE { [ earl:test ?test ; earl:result [ earl:outcome earl:fail ] ] }' | cut -d '<' -f 2 | cut -d '>' -f 1
