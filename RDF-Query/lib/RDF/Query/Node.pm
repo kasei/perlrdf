7e65bbe8c0a9dc99abeabcaeb654c0e15fe07dc5
Gregory Todd Williams <greg@evilfunhouse.com>
2009-01-10 16:27:25 -0500

    - Added RDF::Query::Node::compare for sorting (either Trine or Query) node objects.

--- RDF-Query/lib/RDF/Query/Node.pm
+++ RDF-Query/lib/RDF/Query/Node.pm
@@ -23,6 +23,11 @@ use RDF::Query::Node::Literal;
 use RDF::Query::Node::Resource;
 use RDF::Query::Node::Variable;
 
+our ($VERSION);
+BEGIN {
+	$VERSION	= '2.002';
+}
+
 =item C<< is_variable >>
 
 Returns true if this RDF node is a variable, false otherwise.
@@ -32,6 +37,36 @@ Returns true if this RDF node is a variable, false otherwise.
 sub is_variable {
 	my $self	= shift;
 	return (blessed($self) and $self->isa('RDF::Query::Node::Variable'));
+}
+
+sub compare {
+	my $a	= shift;
+	my $b	= shift;
+	warn 'compare';
+	for ($a, $b) {
+		unless ($_->isa('RDF::Query::Node')) {
+			$_	= RDF::Query::Node->from_trine( $_ );
+		}
+	}
+	
+	local($RDF::Query::Node::Literal::LAZY_COMPARISONS)	= 1;
+	return $a <=> $b;
+}
+
+sub from_trine {
+	my $class	= shift;
+	my $n		= shift;
+	if ($n->isa('RDF::Trine::Node::Variable')) {
+		return RDF::Query::Node::Variable->new( $n->name );
+	} elsif ($n->isa('RDF::Trine::Node::Literal')) {
+		return RDF::Query::Node::Literal->new( $n->literal_value, $n->literal_value_language, $n->literal_datatype );
+	} elsif ($n->isa('RDF::Trine::Node::Resource')) {
+		return RDF::Query::Node::Resource->new( $n->uri_value );
+	} elsif ($n->isa('RDF::Trine::Node::Blank')) {
+		return RDF::Query::Node::Variable->new( $n->blank_identifier );
+	} else {
+		die Dumper($n);
+	}
 }
 
 1;
