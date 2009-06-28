02fd63edd41e4254740f4ec0c91cbe89359e25d2
Gregory Todd Williams <greg@evilfunhouse.com>
2009-06-28 11:20:38 -0400

    - bin/server.pl now takes listening port and data files as CLI arguments.

--- RDF-Endpoint/bin/server.pl
+++ RDF-Endpoint/bin/server.pl
@@ -3,18 +3,25 @@
 use strict;
 use warnings;
 no warnings 'redefine';
-use lib qw(lib ../RDF-Store-DBI/lib ../RDF-SPARQLResults/lib);
+use lib qw(lib ../RDF-Query/lib ../RDF-Store-DBI/lib ../RDF-SPARQLResults/lib);
+
+use RDF::Query::Util;
 use RDF::Endpoint::Server;
+
 $0		= 'sparql-endpoint';
 
 $ENV{TMPDIR}	= '/tmp';
 my $cgi	= CGI->new;
-my $s	= RDF::Endpoint::Server->new(
-			Port		=> 8082,
-			DBServer	=> $ENV{RDFQUERY_DBI_DATABASE} || 'DBI:mysql:database=test',
-			DBUser		=> $ENV{RDFQUERY_DBI_USER} || 'test',
-			DBPass		=> $ENV{RDFQUERY_DBI_PASS} || 'test',
-			Model		=> $ENV{RDFQUERY_DBI_MODEL} || 'endpoint',
+my $port		= 8082;
+if (@ARGV) {
+	if ($ARGV[0] eq '-p') {
+		shift(@ARGV);
+		$port	= shift(@ARGV);
+	}
+}
+my $model = &RDF::Query::Util::cli_make_model;
+my $s	= RDF::Endpoint::Server->new_with_model( $model,
+			Port		=> $port,
 			Prefix		=> '',
 			CGI			=> $cgi,
 		);
