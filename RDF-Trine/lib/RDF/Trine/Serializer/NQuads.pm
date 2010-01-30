16c2a90638aa8c00e5aecbfc079b8a148332c793
Gregory Todd Williams <greg@evilfunhouse.com>
2010-01-30 13:08:12 -0500

    - Added RDF::Trine::Serializer::NQuads.

---
+++ RDF-Trine/lib/RDF/Trine/Serializer/NQuads.pm
@@ -0,0 +1,190 @@
+# RDF::Trine::Serializer::NQuads
+# -----------------------------------------------------------------------------
+
+=head1 NAME
+
+RDF::Trine::Serializer::NQuads - N-Quads Serializer.
+
+=head1 VERSION
+
+This document describes RDF::Trine::Serializer::NQuads version 0.115
+
+=head1 SYNOPSIS
+
+ use RDF::Trine::Serializer::NQuads;
+ my $serializer	= RDF::Trine::Serializer::NQuads->new();
+
+=head1 DESCRIPTION
+
+The RDF::Trine::Serializer::NQuads class provides an API for serializing RDF
+graphs to the N-Quads syntax.
+
+=head1 METHODS
+
+=over 4
+
+=cut
+
+package RDF::Trine::Serializer::NQuads;
+
+use strict;
+use warnings;
+use base qw(RDF::Trine::Serializer);
+
+use URI;
+use Carp;
+use XML::SAX;
+use Data::Dumper;
+use Scalar::Util qw(blessed);
+
+use RDF::Trine::Node;
+use RDF::Trine::Statement;
+use RDF::Trine::Error qw(:try);
+
+######################################################################
+
+our ($VERSION);
+BEGIN {
+	$VERSION	= '0.115';
+}
+
+######################################################################
+
+=item C<< new >>
+
+Returns a new N-Quads serializer object.
+
+=cut
+
+sub new {
+	my $class	= shift;
+	my %args	= @_;
+	my $self = bless( {}, $class);
+	return $self;
+}
+
+=item C<< serialize_model_to_file ( $fh, $model ) >>
+
+Serializes the C<$model> to N-Quads, printing the results to the supplied
+filehandle C<<$fh>>.
+
+=cut
+
+sub serialize_model_to_file {
+	my $self	= shift;
+	my $file	= shift;
+	my $model	= shift;
+	my $iter	= $model->as_stream;
+	while (my $st = $iter->next) {
+		print {$file} $self->_statement_as_string( $st );
+	}
+}
+
+=item C<< serialize_model_to_string ( $model ) >>
+
+Serializes the C<$model> to N-Quads, returning the result as a string.
+
+=cut
+
+sub serialize_model_to_string {
+	my $self	= shift;
+	my $model	= shift;
+	my $iter	= $model->as_stream;
+	my $string	= '';
+	while (my $st = $iter->next) {
+		my @nodes	= $st->nodes;
+		$string		.= $self->_statement_as_string( $st );
+	}
+	return $string;
+}
+
+=item C<< serialize_iterator_to_file ( $file, $iter ) >>
+
+Serializes the iterator to N-Quads, printing the results to the supplied
+filehandle C<<$fh>>.
+
+=cut
+
+sub serialize_iterator_to_file {
+	my $self	= shift;
+	my $file	= shift;
+	my $iter	= shift;
+	while (my $st = $iter->next) {
+		print {$file} $self->_statement_as_string( $st );
+	}
+}
+
+=item C<< serialize_iterator_to_string ( $iter ) >>
+
+Serializes the iterator to N-Quads, returning the result as a string.
+
+=cut
+
+sub serialize_iterator_to_string {
+	my $self	= shift;
+	my $iter	= shift;
+	my $string	= '';
+	while (my $st = $iter->next) {
+		my @nodes	= $st->nodes;
+		$string		.= $self->_statement_as_string( $st );
+	}
+	return $string;
+}
+
+sub _serialize_bounded_description {
+	my $self	= shift;
+	my $model	= shift;
+	my $node	= shift;
+	my $seen	= shift || {};
+	return '' if ($seen->{ $node->sse }++);
+	my $iter	= $model->get_statements( $node, undef, undef );
+	my $string	= '';
+	while (my $st = $iter->next) {
+		my @nodes	= $st->nodes;
+		$string		.= $self->_statement_as_string( $st );
+		if ($nodes[2]->is_blank) {
+			$string	.= $self->_serialize_bounded_description( $model, $nodes[2], $seen );
+		}
+	}
+	return $string;
+}
+
+sub _statement_as_string {
+	my $self	= shift;
+	my $st		= shift;
+	my @nodes;
+	if ($st->type eq 'TRIPLE') {
+		@nodes	= $st->nodes;
+	} else {
+		my $g	= $st->context;
+		if ($g->is_nil) {
+			@nodes	= ($st->nodes)[0..2];
+		} else {
+			@nodes	= $st->nodes;
+		}
+	}
+	return join(' ', map { $_->as_ntriples } @nodes) . " .\n";
+}
+
+
+1;
+
+__END__
+
+=back
+
+=head1 SEE ALSO
+
+L<http://sw.deri.org/2008/07/n-quads/>
+
+=head1 AUTHOR
+
+Gregory Todd Williams  C<< <gwilliams@cpan.org> >>
+
+=head1 COPYRIGHT
+
+Copyright (c) 2006-2010 Gregory Todd Williams. All rights reserved. This
+program is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself.
+
+=cut
---
+++ RDF-Trine/t/serializer-nquads.t
@@ -0,0 +1,105 @@
+use Test::More tests => 5;
+BEGIN { use_ok('RDF::Trine::Serializer::NQuads') };
+
+use strict;
+use warnings;
+
+use RDF::Trine qw(iri blank);
+
+my $store	= RDF::Trine::Store::DBI->temporary_store();
+my $model	= RDF::Trine::Model->new( $store );
+
+my $rdf		= RDF::Trine::Namespace->new('http://www.w3.org/1999/02/22-rdf-syntax-ns#');
+my $foaf	= RDF::Trine::Namespace->new('http://xmlns.com/foaf/0.1/');
+my $kasei	= RDF::Trine::Namespace->new('http://kasei.us/');
+
+my $graph1	= iri('http://example.com/graph1');
+my $graph2	= blank('graph2');
+my $page	= iri('http://kasei.us/');
+my $g		= blank('greg');
+my $st0		= RDF::Trine::Statement->new( $g, $rdf->type, $foaf->Person );
+my $st1		= RDF::Trine::Statement->new( $g, $foaf->name, RDF::Trine::Node::Literal->new('Greg') );
+my $st2		= RDF::Trine::Statement::Quad->new( $g, $foaf->homepage, $page, $graph1 );
+my $st3		= RDF::Trine::Statement::Quad->new( $page, $rdf->type, $foaf->Document, $graph2 );
+$model->add_statement( $_ ) for ($st0, $st1, $st2, $st3);
+
+{
+	my ($rh, $wh);
+	pipe($rh, $wh);
+	my $serializer	= RDF::Trine::Serializer::NQuads->new();
+	$serializer->serialize_model_to_file($wh, $model);
+	close($wh);
+	
+	my %got;
+	while (defined(my $line = <$rh>)) {
+		chomp($line);
+		$got{$line}++;
+	}
+	
+	my $expect	= { map { $_ => 1 } (
+		'_:greg <http://xmlns.com/foaf/0.1/homepage> <http://kasei.us/> <http://example.com/graph1> .',
+		'_:greg <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://xmlns.com/foaf/0.1/Person> .',
+		'_:greg <http://xmlns.com/foaf/0.1/name> "Greg" .',
+		'<http://kasei.us/> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://xmlns.com/foaf/0.1/Document> _:graph2 .',
+	) };
+	
+	is_deeply( \%got, $expect, 'serialize_model_to_file' );
+}
+
+{
+	my $iter	= $model->get_statements( undef, $rdf->type, undef );
+	
+	my ($rh, $wh);
+	pipe($rh, $wh);
+	my $serializer	= RDF::Trine::Serializer::NQuads->new();
+	$serializer->serialize_iterator_to_file($wh, $iter);
+	close($wh);
+	
+	my %got;
+	while (defined(my $line = <$rh>)) {
+		chomp($line);
+		$got{$line}++;
+	}
+	
+	my $expect	= { map { $_ => 1 }
+		'_:greg <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://xmlns.com/foaf/0.1/Person> .',
+		'<http://kasei.us/> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://xmlns.com/foaf/0.1/Document> .',
+	};
+	
+	is_deeply( \%got, $expect, 'serialize_iterator_to_file with triple-based iterator' );
+}
+
+{
+	my $iter	= $model->get_statements( undef, $rdf->type, undef, undef );
+	
+	my ($rh, $wh);
+	pipe($rh, $wh);
+	my $serializer	= RDF::Trine::Serializer::NQuads->new();
+	$serializer->serialize_iterator_to_file($wh, $iter);
+	close($wh);
+	
+	my %got;
+	while (defined(my $line = <$rh>)) {
+		chomp($line);
+		$got{$line}++;
+	}
+	
+	my $expect	= { map { $_ => 1 }
+		'_:greg <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://xmlns.com/foaf/0.1/Person> .',
+		'<http://kasei.us/> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://xmlns.com/foaf/0.1/Document> _:graph2 .',
+	};
+	
+	is_deeply( \%got, $expect, 'serialize_iterator_to_file with quad-based iterator' );
+}
+
+{
+	my $serializer	= RDF::Trine::Serializer::NQuads->new();
+	my $iter		= $model->get_statements( undef, $rdf->type, undef, undef );
+	my $string		= $serializer->serialize_iterator_to_string( $iter );
+	my %got			= map { $_ => 1 } split(/\r?\n/, $string);
+	my $expect	= { map { $_ => 1 }
+		'_:greg <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://xmlns.com/foaf/0.1/Person> .',
+		'<http://kasei.us/> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://xmlns.com/foaf/0.1/Document> _:graph2 .',
+	};
+	is_deeply( \%got, $expect, 'serialize_iterator_to_string with quad-based iterator' );
+}
