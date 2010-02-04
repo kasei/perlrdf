abc21b7dfd69fdc1ee2ef634aa41c6ec129248e4
Gregory Todd Williams <greg@evilfunhouse.com>
2010-02-04 15:01:42 -0500

    - Updated VERSION, SIGNATURE and docs for 0.117 release.

--- RDF-Trine/lib/RDF/Trine/Iterator/SAXHandler.pm
+++ RDF-Trine/lib/RDF/Trine/Iterator/SAXHandler.pm
@@ -7,7 +7,7 @@ RDF::Trine::Iterator::SAXHandler - SAX Handler for parsing SPARQL XML Results fo
 
 =head1 VERSION
 
-This document describes RDF::Trine::Iterator::SAXHandler version 0.116
+This document describes RDF::Trine::Iterator::SAXHandler version 0.117
 
 =head1 SYNOPSIS
 
@@ -35,7 +35,7 @@ use Time::HiRes qw(time);
 
 our ($VERSION);
 BEGIN {
-	$VERSION	= '0.116';
+	$VERSION	= '0.117';
 }
 
 my %strings;
--- RDF-Trine/lib/RDF/Trine/Store.pm
+++ RDF-Trine/lib/RDF/Trine/Store.pm
@@ -7,7 +7,7 @@ RDF::Trine::Store - RDF triplestore base class
 
 =head1 VERSION
 
-This document describes RDF::Trine::Store version 0.116
+This document describes RDF::Trine::Store version 0.117
 
 =cut
 
@@ -31,7 +31,7 @@ use RDF::Trine::Store::SPARQL;
 
 our ($VERSION);
 BEGIN {
-	$VERSION	= '0.116';
+	$VERSION	= '0.117';
 }
 
 ######################################################################
