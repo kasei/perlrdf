012dc50af07e627593d293826b2d95ebcf5a996c
Gregory Todd Williams <greg@evilfunhouse.com>
2010-06-18 17:17:28 -0400

    - Added RDFa service description to HTML page. - Now passes filehandles to $response->body where possible.

--- RDF-Endpoint-Plack/lib/RDF/Endpoint/Plack.pm
+++ RDF-Endpoint-Plack/lib/RDF/Endpoint/Plack.pm
@@ -20,11 +20,13 @@ use RDF::Trine qw(statement iri blank literal);
 
 use Encode;
 use File::Spec;
+use XML::LibXML;
 use Plack::Request;
 use Plack::Response;
 use File::ShareDir qw(dist_dir);
 use HTTP::Negotiate qw(choose);
 use RDF::Trine::Namespace qw(rdf xsd);
+use RDF::RDFa::Generator;
 
 sub new {
 	my $class	= shift;
@@ -46,12 +48,12 @@ sub run {
 		$path		=~ s#^/##;
 		my $file	= File::Spec->catfile(dist_dir('RDF-Endpoint-Plack'), 'www', $path);
 		if (-r $file) {
-			my $content	= do { local($/) = undef; open( my $fh, '<', $file ) or die $!; <$fh> };
+			open( my $fh, '<', $file ) or die $!;
 			$response->status(200);
-			$response->content( encode_utf8($content) );
+			$response->body( $fh );
 		} else {
 			$response->status(404);
-			$response->content(<<"END");
+			$response->body(<<"END");
 <!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">\n<html><head>\n<title>404 Not Found</title>\n</head><body>\n
 <h1>Not Found</h1>\n<p>The requested URL was not found on this server.</p>\n</body></html>
 END
@@ -83,52 +85,59 @@ END
 				if ($stype =~ /html/) {
 					$response->headers->content_type( 'text/plain' );
 					my $content	= $iter->as_string;
-					$response->content( encode_utf8($content) );
+					$response->body( encode_utf8($content) );
 				} elsif ($stype =~ /xml/) {
 					$response->headers->content_type( $stype );
-					$response->content( encode_utf8($iter->as_xml) );
+					$response->body( encode_utf8($iter->as_xml) );
 				} elsif ($stype =~ /json/) {
 					$response->headers->content_type( $stype );
-					$response->content( encode_utf8($iter->as_json) );
+					$response->body( encode_utf8($iter->as_json) );
 				} else {
 					$response->headers->content_type( 'text/plain' );
-					$response->content( encode_utf8($iter->as_string) );
+					$response->body( encode_utf8($iter->as_string) );
 				}
 			} else {
 				$response->status(500);
 			}
 		} else {
 			$response->status(500);
-			$response->content( RDF::Query->error );
+			$response->body( RDF::Query->error );
 		}
 	} else {
 		my @variants;
 		my %media_types	= %RDF::Trine::Serializer::media_types;
 		while (my($type, $sclass) = each(%media_types)) {
+			next if ($type =~ /html/);
 			push(@variants, [$type, 1.0, $type]);
 		}
 		
+		my $ns	= {
+			xsd		=> $xsd->uri->uri_value,
+			sd		=> 'http://www.w3.org/ns/sparql-service-description#',
+			jena	=> 'java:com.hp.hpl.jena.query.function.library.',
+			ldodds	=> 'java:com.ldodds.sparql.',
+			kasei	=> 'http://kasei.us/2007/09/functions/',
+		};
 		push(@variants, ['text/html', 1.0, 'text/html']);
 		my $stype	= choose( \@variants, $headers );
 		my $sdmodel	= service_description( $req, $model );
-		if (my $sclass = $RDF::Trine::Serializer::media_types{ $stype }) {
-			my $s	= $sclass->new( namespaces => {
-				xsd		=> $xsd->uri->uri_value,
-				sd		=> 'http://www.w3.org/ns/sparql-service-description#',
-				jena	=> 'java:com.hp.hpl.jena.query.function.library.',
-				ldodds	=> 'java:com.ldodds.sparql.',
-				kasei	=> 'http://kasei.us/2007/09/functions/',
-			} );
+		if ($stype !~ /html/ and my $sclass = $RDF::Trine::Serializer::media_types{ $stype }) {
+			my $s	= $sclass->new( namespaces => $ns );
 			$response->status(200);
 			$response->headers->content_type($stype);
-			$response->content( encode_utf8($s->serialize_model_to_string($sdmodel)) );
+			$response->body( encode_utf8($s->serialize_model_to_string($sdmodel)) );
 		} else {
 			my $template	= File::Spec->catfile(dist_dir('RDF-Endpoint-Plack'), 'index.html');
-			my $html	= do { local($/) = undef; open( my $fh, '<', $template ) or die $!; <$fh> };
-			
+			my $parser		= XML::LibXML->new();
+			my $doc			= $parser->parse_file( $template );
+			my $gen			= RDF::RDFa::Generator->new( style => 'HTML::Hidden', ns => $ns );
+			$gen->inject_document($doc, $sdmodel);
+			my ($rh, $wh);
+			pipe($rh, $wh);
+			$doc->toFH($wh);
 			$response->status(200);
 			$response->headers->content_type('text/html');
-			$response->content(encode_utf8($html));
+			$response->body($rh);
 		}
 	}
 	return $response;
