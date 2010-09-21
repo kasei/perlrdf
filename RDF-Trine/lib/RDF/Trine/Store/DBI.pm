kasei/perlrdf

commit bc485034fa945026adc64f48eb4ba3fcdb74145b

Updated RDF::Trine::Store::DBI->init to (partially) work if called for a new model (but the database has already been initialized).

RDF-Trine/lib/RDF/Trine/Store/DBI.pm
@@ -1324,6 +1324,18 @@ sub init {
 	my $id		= _mysql_hash( $name );
 	my $l		= Log::Log4perl->get_logger("rdf.trine.store.dbi");
 	
+	$dbh->do( <<"END" ) || do { $l->trace( $dbh->errstr ); return undef };
+        CREATE TABLE Statements${id} (
+            Subject NUMERIC(20) NOT NULL,
+            Predicate NUMERIC(20) NOT NULL,
+            Object NUMERIC(20) NOT NULL,
+            Context NUMERIC(20) NOT NULL DEFAULT 0,
+            PRIMARY KEY (Subject, Predicate, Object, Context)
+        );
+END
+	$dbh->do( "DELETE FROM Models WHERE ID = ${id}") || do { $l->trace( $dbh->errstr ); $dbh->rollback; return undef };
+	$dbh->do( "INSERT INTO Models (ID, Name) VALUES (${id}, ?)", undef, $name ) || do { $l->trace( $dbh->errstr ); $dbh->rollback; return undef };
+	
 	$dbh->begin_work;
 	$dbh->do( <<"END" ) || do { $l->trace( $dbh->errstr ); $dbh->rollback; return undef };
         CREATE TABLE Literals (
@@ -1352,18 +1364,6 @@ END
         );
 END
     
-	$dbh->do( <<"END" ) || do { $l->trace( $dbh->errstr ); $dbh->rollback; return undef };
-        CREATE TABLE Statements${id} (
-            Subject NUMERIC(20) NOT NULL,
-            Predicate NUMERIC(20) NOT NULL,
-            Object NUMERIC(20) NOT NULL,
-            Context NUMERIC(20) NOT NULL DEFAULT 0,
-            PRIMARY KEY (Subject, Predicate, Object, Context)
-        );
-END
-
-	$dbh->do( "DELETE FROM Models WHERE ID = ${id}") || do { $l->trace( $dbh->errstr ); $dbh->rollback; return undef };
-	$dbh->do( "INSERT INTO Models (ID, Name) VALUES (${id}, ?)", undef, $name ) || do { $l->trace( $dbh->errstr ); $dbh->rollback; return undef };
 	
 	$dbh->commit or die $dbh->errstr;
 }

commit 870211bdb25e543891efb82dca6e15d9b4159b2f

Fixed DBI init bug introduced in commit bc48503.

RDF-Trine/lib/RDF/Trine/Store/DBI.pm
@@ -1333,8 +1333,6 @@ sub init {
             PRIMARY KEY (Subject, Predicate, Object, Context)
         );
 END
-	$dbh->do( "DELETE FROM Models WHERE ID = ${id}") || do { $l->trace( $dbh->errstr ); $dbh->rollback; return undef };
-	$dbh->do( "INSERT INTO Models (ID, Name) VALUES (${id}, ?)", undef, $name ) || do { $l->trace( $dbh->errstr ); $dbh->rollback; return undef };
 	
 	$dbh->begin_work;
 	$dbh->do( <<"END" ) || do { $l->trace( $dbh->errstr ); $dbh->rollback; return undef };
@@ -1364,8 +1362,10 @@ END
         );
 END
     
+	$dbh->commit or warn $dbh->errstr;
 	
-	$dbh->commit or die $dbh->errstr;
+	$dbh->do( "DELETE FROM Models WHERE ID = ${id}") || do { $l->trace( $dbh->errstr ); $dbh->rollback; return undef };
+	$dbh->do( "INSERT INTO Models (ID, Name) VALUES (${id}, ?)", undef, $name ) || do { $l->trace( $dbh->errstr ); $dbh->rollback; return undef };
 }
 
 sub _cleanup {