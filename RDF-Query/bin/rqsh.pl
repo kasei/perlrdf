82d3a9468bf0c9c17811d8b34df678ad730d8cb6
Gregory Todd Williams <greg@evilfunhouse.com>
2010-09-21 13:36:28 -0400

    Refactored code in bin/rqsh.pl.

--- RDF-Query/bin/rqsh.pl
+++ RDF-Query/bin/rqsh.pl
@@ -39,105 +39,127 @@ unless (exists $args{update}) {
 $args{ base }	= 'file://' . getcwd . '/';
 
 my $class	= delete $args{ class } || 'RDF::Query';
-my $term	= Term::ReadLine->new('rqsh');
+my $term	= Term::ReadLine->new('rqsh', \*STDIN, \*STDOUT);
 
+print "rqsh v1.0\n\n";
 while ( defined ($_ = $term->readline('rqsh> ')) ) {
 	my $sparql	= $_;
 	next unless (length($sparql));
-	if ($sparql eq 'debug') {
-		print "# model = $model\n";
-		if (my $store = $model->_store) {
-			print "# store = $store\n";
-		}
-		my $iter	= $model->get_statements( undef, undef, undef, undef );
-		my @rows;
-		my @names	= qw[subject predicate object context];
-		while (my $row = $iter->next) {
-			push(@rows, [map {$row->$_()->as_string} @names]);
-		}
-		my @rule			= qw(- +);
-		my @headers			= (\q"| ");
-		push(@headers, map { $_ => \q" | " } @names);
-		pop	@headers;
-		push @headers => (\q" |");
-		my $table = Text::Table->new(@names);
-		$table->rule(@rule);
-		$table->body_rule(@rule);
-		$table->load(@rows);
-		print join('',
-				$table->rule(@rule),
-				$table->title,
-				$table->rule(@rule),
-				map({ $table->body($_) } 0 .. @rows),
-				$table->rule(@rule)
-			);
-		my $size	= scalar(@rows);
-		print "$size statements\n";
-	} elsif ($sparql =~ /^use (\w+)\s*;?\s*$/) {
+	if ($sparql =~ /^use (\w+)\s*;?\s*$/) {
 		my $name	= $1;
-		my $sclass	= RDF::Trine::Store->class_by_name( $name );
-		if ($sclass) {
-			if ($sclass eq 'RDF::Trine::Store::Memory') {
-				$model	= memory_model();
-				next;
-			} else {
-				if ($sclass->can('_config_meta')) {
-					my $meta	= $sclass->_config_meta;
-					my $keys	= $meta->{required_keys};
-					my $config	= {};
-					foreach my $k (@$keys) {
-						get_value( $meta, $k, $config );
-					}
-					my $store	= eval { $sclass->new_with_config( $config ) };
-					if ($store) {
-						my $m		= RDF::Trine::Model->new( $store );
-						if ($m) {
-							$model	= $m;
-							next;
-						}
-					}
-					print "Failed to construct '$name'-backed model.\n";
-					next;
-				} else {
-					print "Cannot construct model from '$name' storage class.\n";
-				}
-			}
-		} else {
-			print "No storage class named '$name' found\n";
-			next;
+		my $nmodel	= model( $name );
+		if ($nmodel) {
+			$model	= $nmodel;
 		}
+	} elsif ($sparql eq 'debug') {
+		debug( $model, $term, $sparql );
 	} else {
-		my $psparql	= join("\n", $RDF::Query::Util::PREFIXES, $sparql);
-		my $query	= $class->new( $psparql, \%args );
-		unless ($query) {
-			print "Error: " . RDF::Query->error . "\n";
-			next;
-		}
-		$term->addhistory($sparql);
-		try {
-			my ($plan, $ctx)	= $query->prepare($model);
-			my $iter	= $query->execute_plan( $plan, $ctx );
-			my $count	= -1;
-			if (blessed($iter)) {
-				print $iter->as_string( 0, \$count );
-			}
-			if ($plan->is_update) {
-				my $size	= $model->size;
-				print "$size statements\n";
-			} elsif ($count >= 0) {
-				print "$count results\n";
-			}
-		} catch RDF::Query::Error with {
-			my $e	= shift;
-			print "Error: $e\n";
-		} otherwise {
-			warn "died: " . Dumper(\@_);
-		};
+		query( $model, $term, $sparql );
 	}
 }
 
+sub model {
+	my $name	= shift;
+	my $sclass	= RDF::Trine::Store->class_by_name( $name );
+	if ($sclass) {
+		if ($sclass eq 'RDF::Trine::Store::Memory') {
+			$model	= memory_model();
+			return;
+		} else {
+			if ($sclass->can('_config_meta')) {
+				my $meta	= $sclass->_config_meta;
+				my $keys	= $meta->{required_keys};
+				my $config	= {};
+				foreach my $k (@$keys) {
+					get_value( $term, $meta, $k, $config );
+				}
+				my $store	= eval { $sclass->new_with_config( $config ) };
+				if ($store) {
+					my $m		= RDF::Trine::Model->new( $store );
+					if ($m) {
+						return $m;
+					}
+				}
+				print "Failed to construct '$name'-backed model.\n";
+				return;
+			} else {
+				print "Cannot construct model from '$name' storage class.\n";
+			}
+		}
+	} else {
+		print "No storage class named '$name' found\n";
+		return;
+	}
+}
+
+sub query {
+	my $model	= shift;
+	my $term	= shift;
+	my $sparql	= shift;
+	my $psparql	= join("\n", $RDF::Query::Util::PREFIXES, $sparql);
+	my $query	= $class->new( $psparql, \%args );
+	unless ($query) {
+		print "Error: " . RDF::Query->error . "\n";
+		return;
+	}
+	$term->addhistory($sparql);
+	try {
+		my ($plan, $ctx)	= $query->prepare($model);
+		my $iter	= $query->execute_plan( $plan, $ctx );
+		my $count	= -1;
+		if (blessed($iter)) {
+			print $iter->as_string( 0, \$count );
+		}
+		if ($plan->is_update) {
+			my $size	= $model->size;
+			print "$size statements\n";
+		} elsif ($count >= 0) {
+			print "$count results\n";
+		}
+	} catch RDF::Query::Error with {
+		my $e	= shift;
+		print "Error: $e\n";
+	} otherwise {
+		warn "died: " . Dumper(\@_);
+	};
+}
+
+sub debug {
+	my $model	= shift;
+	my $term	= shift;
+	my $line	= shift;
+	print "# model = $model\n";
+	if (my $store = $model->_store) {
+		print "# store = $store\n";
+	}
+	my $iter	= $model->get_statements( undef, undef, undef, undef );
+	my @rows;
+	my @names	= qw[subject predicate object context];
+	while (my $row = $iter->next) {
+		push(@rows, [map {$row->$_()->as_string} @names]);
+	}
+	my @rule			= qw(- +);
+	my @headers			= (\q"| ");
+	push(@headers, map { $_ => \q" | " } @names);
+	pop	@headers;
+	push @headers => (\q" |");
+	my $table = Text::Table->new(@names);
+	$table->rule(@rule);
+	$table->body_rule(@rule);
+	$table->load(@rows);
+	print join('',
+			$table->rule(@rule),
+			$table->title,
+			$table->rule(@rule),
+			map({ $table->body($_) } 0 .. @rows),
+			$table->rule(@rule)
+		);
+	my $size	= scalar(@rows);
+	print "$size statements\n";
+}
 
 sub get_value {
+	my $term	= shift;
 	my $meta	= shift;
 	my $k		= shift;
 	my $config	= shift;
@@ -149,7 +171,7 @@ sub get_value {
 		my $template	= $meta->{fields}{$k}{'template'};
 		my @subkeys	= ($template =~ m/\[%(\w+)%\]/g);
 		foreach my $sk (@subkeys) {
-			get_value( $meta, $sk, $config );
+			get_value( $term, $meta, $sk, $config );
 		}
 		while ($template =~ m/\[%(\w+)%\]/) {
 			my $key	= $1;
@@ -164,17 +186,14 @@ sub get_value {
 		if ($type eq 'password') {
 			print "$desc: ";
 			ReadMode('noecho');
-			$value	= ReadLine(0);
+			$value	= ReadLine(0, $term->IN);
 			chomp($value);
-			warn "password: $value";
 		} elsif ($type eq 'filename') {
 			my $attribs	= $term->Attribs;
-			$attribs->{completion_entry_function}	= $attribs->{filename_completion_function};
+			local($attribs->{completion_entry_function})	= $attribs->{filename_completion_function};
 			$value	= $term->readline("$desc: ");
 		} else {
-			print "$desc: ";
-			$value	= <>;
-			chomp($value);
+			$value = $term->readline("$desc: ")
 		}
 		$config->{ $k }	= $value;
 	}
