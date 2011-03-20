2300b9a6fa3320e61276349b169b0a0bf6a8e5c7
Gregory Todd Williams <greg@evilfunhouse.com>
2011-03-19 22:46:20 -0400

    Fixed RDF::Trine::Node::Resource->qname handling of unicode IRIs.

--- RDF-Trine/lib/RDF/Trine/Node/Resource.pm
+++ RDF-Trine/lib/RDF/Trine/Node/Resource.pm
@@ -260,11 +260,12 @@ QName, returns a list containing these two parts. Otherwise throws an exception.
 sub qname {
 	my $p		= shift;
 	my $uri		= $p->uri_value;
-	
-	my $nameStartChar	= qr<([A-Za-z_]|[\x{C0}-\x{D6}]|[\x{D8}-\x{D8}]|[\x{F8}-\x{F8}]|[\x{200C}-\x{200C}]|[\x{37F}-\x{1FFF}][\x{200C}-\x{200C}]|[\x{2070}-\x{2070}]|[\x{2C00}-\x{2C00}]|[\x{3001}-\x{3001}]|[\x{F900}-\x{F900}]|[\x{FDF0}-\x{FDF0}]|[\x{10000}-\x{10000}])>;
-	my $nameChar		= qr<$nameStartChar|-|[.]|[0-9]|\x{B7}|[\x{0300}-\x{036F}]|[\x{203F}-\x{2040}]>;
-	my $lnre			= qr<((${nameStartChar})($nameChar)*)>;
-	if ($uri =~ m/${lnre}$/) {
+
+	our $r_PN_CHARS_BASE	||= qr/([A-Z]|[a-z]|[\x{00C0}-\x{00D6}]|[\x{00D8}-\x{00F6}]|[\x{00F8}-\x{02FF}]|[\x{0370}-\x{037D}]|[\x{037F}-\x{1FFF}]|[\x{200C}-\x{200D}]|[\x{2070}-\x{218F}]|[\x{2C00}-\x{2FEF}]|[\x{3001}-\x{D7FF}]|[\x{F900}-\x{FDCF}]|[\x{FDF0}-\x{FFFD}]|[\x{10000}-\x{EFFFF}])/;
+	our $r_PN_CHARS_U		||= qr/(_|${r_PN_CHARS_BASE})/;
+	our $r_PN_CHARS			||= qr/${r_PN_CHARS_U}|-|[0-9]|\x{00B7}|[\x{0300}-\x{036F}]|[\x{203F}-\x{2040}]/;
+	our $r_PN_LOCAL			||= qr/((${r_PN_CHARS_U})((${r_PN_CHARS}|[.])*${r_PN_CHARS})?)/;
+	if ($uri =~ m/${r_PN_LOCAL}$/) {
 		my $ln	= $1;
 		my $ns	= substr($uri, 0, length($uri)-length($ln));
 		return ($ns, $ln);
