534680b393ef0c7fc1aa0a0a3b49cbdebd26098f
Gregory Todd Williams <greg@evilfunhouse.com>
2011-03-30 17:18:25 +0100

    Added ability to forcibly remove a particular join algorithm from consideration in plan generation. Added support for empty Sequence operations (allowing no-op updates).

--- RDF-Query/lib/RDF/Query/Plan.pm
+++ RDF-Query/lib/RDF/Query/Plan.pm
@@ -169,7 +169,7 @@ sub explain {
 				$string	.= $p->explain( $s, $count+1 );
 			}
 		} elsif (ref($p)) {
-			die 'unexpected non-blessed ref in RDF::Query::Plan->explain: ' . Dumper($p);
+			warn 'unexpected non-blessed ref in RDF::Query::Plan->explain: ' . Dumper($p);
 		} else {
 			$string	.= "${indent}${s}$p\n";
 		}
@@ -398,6 +398,8 @@ sub generate_plans {
 	my $class	= ref($self) || $self;
 	my $algebra	= shift;
 	my $context	= shift;
+	my $config	= $context->options || {};
+	
 	my %args	= @_;
 	my $active_graph	= $args{ active_graph } || RDF::Trine::Node::Nil->new();
 	
@@ -525,7 +527,7 @@ sub generate_plans {
 			foreach my $t (@csg_triples) {
 				push(@csg_plans, [ $self->generate_plans( $t, $context, %args ) ]);
 			}
-			my @join_types	= RDF::Query::Plan::Join->join_classes;
+			my @join_types	= RDF::Query::Plan::Join->join_classes( $config );
 			while (my $cps = shift(@csg_plans)) {
 				my @temp_plans	= @plans;
 				@plans			= ();
@@ -578,7 +580,7 @@ sub generate_plans {
 		# just like a BGP or GGP, but we have to pass the optional flag to the join constructor
 		my @patterns	= ($algebra->pattern, $algebra->optional);
 		my @base_plans	= map { [ $self->generate_plans( $_, $context, %args ) ] } @patterns;
-		my @join_types	= RDF::Query::Plan::Join->join_classes;
+		my @join_types	= RDF::Query::Plan::Join->join_classes( $config );
 		# XXX this is currently only considering left-deep trees. maybe it should produce all trees?
 		my @plans;
 		my $base_a	= shift(@base_plans);
@@ -622,7 +624,7 @@ sub generate_plans {
 			# if there's constant data to be joined, we better do it now in case
 			# the project gets rid of variables needed for the join
 			my @plans	= splice( @base );
-			@base		= $self->_add_constant_join( $constant, @plans );
+			@base		= $self->_add_constant_join( $context, $constant, @plans );
 			$constant	= undef;
 		}
 		
@@ -722,9 +724,15 @@ sub generate_plans {
 		my $plan	= RDF::Query::Plan::Union->new( map { $_->[0] } @plans );
 		push(@return_plans, $plan);
 	} elsif ($type eq 'Sequence') {
-		my @plans	= map { [ $self->generate_plans( $_, $context, %args ) ] } $algebra->patterns;
-		my $plan	= RDF::Query::Plan::Sequence->new( map { $_->[0] } @plans );
-		push(@return_plans, $plan);
+		my @pat		= $algebra->patterns;
+		if (@pat) {
+			my @plans	= map { [ $self->generate_plans( $_, $context, %args ) ] } @pat;
+			my $plan	= RDF::Query::Plan::Sequence->new( map { $_->[0] } @plans );
+			push(@return_plans, $plan);
+		} else {
+			my $stream	= RDF::Trine::Iterator::Bindings->new( sub {} );
+			push(@return_plans, $stream);
+		}
 	} elsif ($type eq 'Load') {
 		push(@return_plans, RDF::Query::Plan::Load->new( $algebra->url, $algebra->graph ));
 	} elsif ($type eq 'Update') {
@@ -763,7 +771,7 @@ sub generate_plans {
 	
 	if ($constant and scalar(@$constant)) {
 		my @plans		= splice( @return_plans );
-		@return_plans	= $self->_add_constant_join( $constant, @plans );
+		@return_plans	= $self->_add_constant_join( $context, $constant, @plans );
 	}
 	
 	foreach my $p (@return_plans) {
@@ -797,9 +805,10 @@ sub _join_plans {
 	my $context	= shift;
 	my $triples	= shift;
 	my %args	= @_;
+	my $config	= $context->options || {};
 	
 	my $method		= $args{ method };
-	my @join_types	= RDF::Query::Plan::Join->join_classes;
+	my @join_types	= RDF::Query::Plan::Join->join_classes( $config );
 	
 	my @plans;
 	my $opt		= $context->optimize;
@@ -872,9 +881,11 @@ sub _join_plans {
 
 sub _add_constant_join {
 	my $self		= shift;
+	my $context		= shift;
 	my $constant	= shift;
 	my @return_plans	= @_;
-	my @join_types	= RDF::Query::Plan::Join->join_classes;
+	my $config		= $context->options || {};
+	my @join_types	= RDF::Query::Plan::Join->join_classes( $config );
 	while (my $const = shift(@$constant)) {
 		my @plans	= splice(@return_plans);
 		foreach my $p (@plans) {
@@ -915,6 +926,7 @@ sub __path_plan {
 	my $graph	= shift;
 	my $context	= shift;
 	my %args	= @_;
+	my $config		= $context->options || {};
 	my $l		= Log::Log4perl->get_logger("rdf.query.plan.path");
 	if (blessed($path)) {
 		my $s	= $start;
@@ -1010,7 +1022,7 @@ sub __path_plan {
 				push(@plans, $rhs);
 				$joinvar	= $endvar;
 			}
-			my @join_types	= RDF::Query::Plan::Join->join_classes;
+			my @join_types	= RDF::Query::Plan::Join->join_classes( $config );
 			my @jplans;
 			foreach my $jclass (@join_types) {
 				push(@jplans, $jclass->new( @plans[0,1], 0 ));
@@ -1042,7 +1054,7 @@ sub __path_plan {
 				push(@plans, $rhs);
 				$joinvar	= $endvar;
 			}
-			my @join_types	= RDF::Query::Plan::Join->join_classes;
+			my @join_types	= RDF::Query::Plan::Join->join_classes( $config );
 			my @jplans;
 			
 			my @plan	= shift(@plans);
