8c2618c03b02118dccef5776ed17355fe1071f74
Gregory Todd Williams <greg@evilfunhouse.com>
2011-04-16 23:13:43 -0400

    Updated VERSION and docs for 0.02 release.

--- RDF-Endpoint/lib/RDF/Endpoint.pm
+++ RDF-Endpoint/lib/RDF/Endpoint.pm
@@ -4,7 +4,7 @@ RDF::Endpoint - A SPARQL Protocol Endpoint implementation
 
 =head1 VERSION
 
-This document describes RDF::Endpoint version 0.01_03.
+This document describes RDF::Endpoint version 0.02.
 
 =head1 SYNOPSIS
 
@@ -83,7 +83,7 @@ package RDF::Endpoint;
 use 5.008;
 use strict;
 use warnings;
-our $VERSION	= '0.01_04';
+our $VERSION	= '0.02';
 
 use RDF::Query 2.905;
 use RDF::Trine 0.134 qw(statement iri blank literal);
