ffad1695749d502fb3ebb979d5481d3c0725e31d
Gregory Todd Williams <greg@evilfunhouse.com>
2011-07-13 09:32:30 -0700

    Return an empty string instead of undef in RDF::Trine::Iterator->format_node_xml.

--- RDF-Trine/lib/RDF/Trine/Iterator.pm
+++ RDF-Trine/lib/RDF/Trine/Iterator.pm
@@ -401,7 +401,7 @@ sub format_node_xml ($$$$) {
 	my $node_label;
 	
 	if(!defined $node) {
-		return;
+		return '';
 	} elsif ($node->is_resource) {
 		$node_label	= $node->uri_value;
 		$node_label	=~ s/&/&amp;/g;
