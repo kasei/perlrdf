9ad433e71a513ddabd3790c326979907e718d611
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2011-10-27 10:35:04 +0200

    Check the Perl version when running Tiny::UUID

--- RDF-Trine/lib/RDF/Trine/Parser.pm
+++ RDF-Trine/lib/RDF/Trine/Parser.pm
@@ -385,7 +385,7 @@ a globally unique bnode prefix. Otherwise, an empty string is returned.
 
 sub new_bnode_prefix {
 	my $class	= shift;
-	if (defined($UUID::Tiny::VERSION)) {
+	if (defined($UUID::Tiny::VERSION) && ($] < 5.014000)) {
 		no strict 'subs';
 		my $uuid	= UUID::Tiny::create_UUID_as_string(UUID::Tiny::UUID_V1);
 		$uuid		=~ s/-//g;
