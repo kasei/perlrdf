0d005923f153d789e02f41bf7b38a123a9f17da1
Toby Inkster <tobyink@cpan.org>
2012-08-28 10:21:31 +0100

    Documentation for canonicalization.

--- RDF-Trine/lib/RDF/Trine/Node/API/Canonicalize.pm
+++ RDF-Trine/lib/RDF/Trine/Node/API/Canonicalize.pm
@@ -56,3 +56,55 @@ sub canonicalize
 
 __END__
 
+=head1 NAME
+
+RDF::Trine::Node::API::Canonicalize - role for literals with datatypes that can be canonicalized
+
+=head1 DESCRIPTION
+
+=head2 Requires
+
+This role requires consuming classes to implement the following methods:
+
+=over
+
+=item C<< value >>
+
+=item C<< datatype >>
+
+=item C<< _build_is_valid_lexical_form >>
+
+=item C<< _build_canonical_lexical_form >>
+
+=back
+
+=head2 Methods
+
+This role provides the following methods:
+
+=over
+
+=item C<< is_valid_lexical_form >>
+
+Returns true if the literal value is lexically valid for its datatype.
+
+=item C<< is_canonical_lexical_form >>
+
+Returns true if the literal value is canonical for its datatype.
+
+=item C<< canonical_lexical_form >>
+
+Returns the canonical lexical form for the literal value, as a string.
+
+For example, the decimal number C<< 1.10 >> would be canonicalized as C<< 1.1 >>.
+
+=item C<< canonicalize >>
+
+Like C<< canonical_lexical_form >> but returns a new
+L<RDF::Trine::Node::Literal> object.
+
+=back
+
+
+
+
