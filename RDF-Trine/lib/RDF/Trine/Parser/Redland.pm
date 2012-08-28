d3d19dda347e505b9a1a1df9183b1ad8b8004d66
Gregory Todd Williams <greg@evilfunhouse.com>
2012-08-28 17:15:27 +0200

    Fix RDF::Trine::Parser::Redland to use new API for literal canonicalization.

--- RDF-Trine/lib/RDF/Trine/Parser/Redland.pm
+++ RDF-Trine/lib/RDF/Trine/Parser/Redland.pm
@@ -177,12 +177,7 @@ sub parse {
 			my $o = $stmt->object;
 			# basically copied from RDF::Trine::Parser::Turtle
 			if ($o->isa('RDF::Trine::Node::Literal') and $o->has_datatype) {
-				my $value	= $o->literal_value;
-				my $dt		= $o->literal_datatype;
-				my $canon	= RDF::Trine::Node::Literal->canonicalize_literal_value( $value, $dt, 1 );
-				$o	= literal( $canon, undef, $dt );
-
-				$stmt->object($o);
+				$stmt->object($o->canonicalize);
 			}
 		}
 
