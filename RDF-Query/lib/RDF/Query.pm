5fd99bb6188fb6d983a9eff8896820b67f572a96
Gregory Todd Williams <greg@evilfunhouse.com>
2014-01-24 11:26:38 -0800

    Disable pushing full updates down into the store object (only meant to be full queries at this point).

--- RDF-Query/lib/RDF/Query.pm
+++ RDF-Query/lib/RDF/Query.pm
@@ -579,18 +579,20 @@ sub query_plan {
 	my @bkeys	= keys %{ $bound };
 	my $model	= $context->model;
 	
-	if (not exists $self->{options}{'rdf.query.plan.delegate'} or $self->{options}{'rdf.query.plan.delegate'}) {
-		my $delegate_key	= $self->{update}
-							? 'http://www.w3.org/ns/sparql-service-description#SPARQL11Update'
-							: "http://www.w3.org/ns/sparql-service-description#SPARQL10Query";	# TODO: need to determine if the query is only 1.0, and if so, check for 1.0 support. otherwise check for 1.1 support
-		if (scalar(@bkeys) == 0 and $model->supports($delegate_key)) {
-			my $plan	= RDF::Query::Plan::Iterator->new( sub {
-				my $context	= shift;
-				my $model	= $context->model;
-				my $iter	= $model->get_sparql( $self->{query_string} );
-				return $iter;
-			} );
-			return $plan;
+	unless ($self->{update}) {
+		if (not exists $self->{options}{'rdf.query.plan.delegate'} or $self->{options}{'rdf.query.plan.delegate'}) {
+			my $delegate_key	= $self->{update}
+								? 'http://www.w3.org/ns/sparql-service-description#SPARQL11Update'
+								: "http://www.w3.org/ns/sparql-service-description#SPARQL10Query";	# TODO: need to determine if the query is only 1.0, and if so, check for 1.0 support. otherwise check for 1.1 support
+			if (scalar(@bkeys) == 0 and $model->supports($delegate_key)) {
+				my $plan	= RDF::Query::Plan::Iterator->new( sub {
+					my $context	= shift;
+					my $model	= $context->model;
+					my $iter	= $model->get_sparql( $self->{query_string} );
+					return $iter;
+				} );
+				return $plan;
+			}
 		}
 	}
 	
