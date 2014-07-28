c89bab1a133075c7cea65071604f069298104126
Gregory Todd Williams <greg@evilfunhouse.com>
2014-07-28 15:21:52 -0700

    Make RDF::Trine::VariableBindings->join more flexible in testing of equality.

--- RDF-Trine/lib/RDF/Trine/VariableBindings.pm
+++ RDF-Trine/lib/RDF/Trine/VariableBindings.pm
@@ -108,7 +108,7 @@ sub join {
 		my $val_a	= $self->{ $key };
 		my $val_b	= $rowb->{ $key };
 		next unless (defined($val_a) and defined($val_b));
-		my $equal	= (refaddr($val_a) == refaddr($val_b)) || $val_a->equal( $val_b );
+		my $equal	= (refaddr($val_a) == refaddr($val_b)) || ($val_a == $val_b) || $val_a->equal( $val_b );
 		unless ($equal) {
 			return;
 		}
