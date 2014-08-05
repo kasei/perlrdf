36c49e6242870bc1558437cf271a29d4106c9509
Dorian Taylor <dorian.taylor.lists@gmail.com>
2014-08-05 05:57:20 -0700

    adding notice to multiplicity block

--- RDF-Trine/lib/RDF/Trine/Store/SPARQL.pm
+++ RDF-Trine/lib/RDF/Trine/Store/SPARQL.pm
@@ -274,6 +274,9 @@ sub get_statements {
 				or (blessed($nodes[$i]) and $nodes[$i]->is_variable)) {
 			$nodes[$i]	= RDF::Trine::Node::Variable->new( $var_map[ $i ] );
 		}
+        # This block represents the node multiplicity/cartesian
+        # product form of get_statements which will be introduced in
+        # another branch. -djt
 		elsif (ref $nodes[$i] eq 'ARRAY') {
 			throw RDF::Trine::Error::MethodInvocationError
 				-text => "ARRAY ref contents must be non-variable Node objects"
@@ -297,6 +300,7 @@ sub get_statements {
 				$nodes[$i] = RDF::Trine::Node::Variable->new( $var_map[ $i ] );
 			}
 		}
+        # End multiplicity block.
 		elsif (blessed($nodes[$i]) and $nodes[$i]->isa('RDF::Trine::Node')) {
 			# noop
 		}
