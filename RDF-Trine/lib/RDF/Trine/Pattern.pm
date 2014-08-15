0a74ab4eda9ebb892dac676f7ec303b383db3bf1
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2014-08-15 16:10:50 +0200

    remove commented-out code

--- RDF-Trine/lib/RDF/Trine/Pattern.pm
+++ RDF-Trine/lib/RDF/Trine/Pattern.pm
@@ -178,7 +178,7 @@ that they may be joined in order while avoiding cartesian products (if possible)
 sub sort_for_join_variables {
 	my $self	= shift;
 	my $class	= ref($self);
-	my @triples	= $self->triples; # T in HSP
+	my @triples	= $self->triples;
 	my $l		= Log::Log4perl->get_logger("rdf.trine.pattern");
 	$l->debug('Reordering ' . scalar @triples . ' triples for heuristical optimizations');
 	my %structure_counts;
@@ -244,48 +244,6 @@ sub sort_for_join_variables {
 	}
 
 	return $class->new(@sorted_triples);
-
-
-	# foreach my $var (keys %triples_with_variable) {
-	# 	my @tids	= sort { $a <=> $b } keys %{ $triples_with_variable{ $var } };
-	# 	$triples_with_variable{ $var }	= \@tids;
-	# }
-	
-	# my %variables_in_triple;
-	# foreach my $var (keys %triples_with_variable) {
-	# 	foreach my $tid (@{ $triples_with_variable{ $var } }) {
-	# 		$variables_in_triple{ $tid }{ $var }++;
-	# 	}
-	# }
-	# foreach my $tid (keys %variables_in_triple) {
-	# 	my @vars	= sort keys %{ $variables_in_triple{ $tid } };
-	# 	$variables_in_triple{ $tid }	= \@vars;
-	# }
-	
-	
-	# my %used_vars;
-	# my %used_tids;
-	# my @sorted;
-	# my $first	= shift(@triples);	# start with the first triple in syntactic order
-	# push(@sorted, $first);
-	# $used_tids{ refaddr($first) }++;
-	# foreach my $var (@{ $variables_in_triple{ refaddr($first) } }) {
-	# 	$used_vars{ $var }++;
-	# }
-	# while (@triples) {
-	# 	my @candidate_tids	= grep { not($used_tids{$_}) } map { @{ $triples_with_variable{ $_ } } } (keys %used_vars);
-	# 	last unless scalar(@candidate_tids);
-	# 	my $next_id	= shift(@candidate_tids);
-	# 	my $next	= $triples_by_tid{ $next_id };
-	# 	push(@sorted, $next);
-	# 	$used_tids{ refaddr($next) }++;
-	# 	foreach my $var (@{ $variables_in_triple{ refaddr($next) } }) {
-	# 		$used_vars{ $var }++;
-	# 	}
-	# 	@triples	= grep { refaddr($_) != $next_id } @triples;
-	# }
-	# push(@sorted, @triples);
-	# return $class->new(@sorted);
 }
 
 sub _hsp_heuristic_1_4_triple_pattern_order { # Heuristic 1 and 4 of HSP
