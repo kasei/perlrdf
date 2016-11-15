3aa82e6164e930db4cb3ed49d2644588af3b0671
Gregory Todd Williams <greg@evilfunhouse.com>
2016-11-15 10:26:26 -0800

    Updated rdf_store_add_file.pl to guess the appropriate parser based on the filename.

--- RDF-Trine/bin/rdf_store_add_file.pl
+++ RDF-Trine/bin/rdf_store_add_file.pl
@@ -83,6 +83,7 @@ if ($file =~ qr[^http(s?)://]) {
 # }
 # 
 
-my $parser	= RDF::Trine::Parser::RDFXML->new();
+my $pclass	= RDF::Trine::Parser->guess_parser_by_filename( $file );
+my $parser	= $pclass->new();
 my $m		= RDF::Trine::Model->new( $store );
 $parser->parse_into_model( $base, $data, $m );
