58c081b0f162f9956782521506203fb6e8154dc9
Gregory Todd Williams <greg@evilfunhouse.com>
2017-01-04 09:17:02 -0800

    Updated docs and VERSION for 1.015 release.

--- RDF-Trine/lib/RDF/Trine/Model.pm
+++ RDF-Trine/lib/RDF/Trine/Model.pm
@@ -7,7 +7,7 @@ RDF::Trine::Model - Model class
 
 =head1 VERSION
 
-This document describes RDF::Trine::Model version 1.014
+This document describes RDF::Trine::Model version 1.015
 
 =head1 METHODS
 
@@ -23,7 +23,7 @@ no warnings 'redefine';
 
 our ($VERSION);
 BEGIN {
-	$VERSION	= '1.014';
+	$VERSION	= '1.015';
 }
 
 use Scalar::Util qw(blessed refaddr);
