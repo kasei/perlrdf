62e5efe808dc3e2a116b8722c715adcd320db87a
Gregory Todd Williams <greg@evilfunhouse.com>
2018-01-04 21:00:25 -0800

    Updated docs and VERSION for 1.019 release.

--- RDF-Trine/lib/RDF/Trine/Node/Resource.pm
+++ RDF-Trine/lib/RDF/Trine/Node/Resource.pm
@@ -7,7 +7,7 @@ RDF::Trine::Node::Resource - RDF Node class for IRI resources
 
 =head1 VERSION
 
-This document describes RDF::Trine::Node::Resource version 1.018
+This document describes RDF::Trine::Node::Resource version 1.019
 
 =cut
 
@@ -30,7 +30,7 @@ use Carp qw(carp croak confess);
 
 our ($VERSION, %sse, %ntriples);
 BEGIN {
-	$VERSION	= '1.018';
+	$VERSION	= '1.019';
 }
 
 ######################################################################
--- RDF-Trine/lib/RDF/Trine/Parser/RDFXML.pm
+++ RDF-Trine/lib/RDF/Trine/Parser/RDFXML.pm
@@ -7,7 +7,7 @@ RDF::Trine::Parser::RDFXML - RDF/XML Parser
 
 =head1 VERSION
 
-This document describes RDF::Trine::Parser::RDFXML version 1.018
+This document describes RDF::Trine::Parser::RDFXML version 1.019
 
 =head1 SYNOPSIS
 
@@ -53,7 +53,7 @@ use RDF::Trine::Error qw(:try);
 
 our ($VERSION, $HAS_XML_LIBXML);
 BEGIN {
-	$VERSION	= '1.018';
+	$VERSION	= '1.019';
 	$RDF::Trine::Parser::parser_names{ 'rdfxml' }	= __PACKAGE__;
 	foreach my $ext (qw(rdf xrdf rdfx)) {
 		$RDF::Trine::Parser::file_extensions{ $ext }	= __PACKAGE__;
